6a3f42235823c053f4e390e2b0451fe8904e1c6f
iskandarra <97927328+iskandarra@users.noreply.github.com>
2022-01-18 09:55:15 +0700

    Create HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,7 @@ public class HelloWorld{
   public static void main (){
     System.out.println("Hello World");
     System.out.print("Sukses Mulia");
-    System.out.println("Hiduplah sehidup-hidupnya, Jauhkan dari kata depresi dan frustasi
-                       \nagar Hidup Ini menjadi indah, maka warnailah dengan Good day Setiap hari karena Good day punya banyak rasa");
+    System.out.print("sehat selalau");
   }
 
 }
